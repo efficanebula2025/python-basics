e81ddee4b50d3cb0d2f01bd4e0129fcc830002c7
efficanebula2025 <efficanebula@gmail.com>
2025-09-18 12:42:13 +0530

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,10 @@
+# Teaching the robot to remember its name
+robot_name = "Buddy"
+print("My name is", robot_name)
+
+
+# How this works:
+# robot_name is like a box with a label
+# = means "put this information in the box"
+# "Buddy" is the information we're storing
+# Later, when we say robot_name, the robot looks in that box and finds "Buddy"
