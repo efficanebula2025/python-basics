519e0de49f231c2fba74fb5dfe45c71cadb91d54
efficanebula2025 <efficanebula@gmail.com>
2025-09-18 12:43:18 +0530

    Create Multiple_variables.py

---
+++ Multiple_variables.py
@@ -0,0 +1,17 @@
+# Robot's basic information
+robot_name = "Buddy"
+robot_age = 2
+robot_battery = 85
+robot_can_move = True
+
+print("Hi! My name is", robot_name)
+print("I am", robot_age, "years old")
+print("My battery is at", robot_battery, "percent")
+print("Can I move?", robot_can_move)
+
+
+# How this works:
+# robot_age = 2 stores a number (no quotes needed for numbers)
+# robot_battery = 85 stores another number
+# robot_can_move = True stores a yes/no answer (True = yes, False = no)
+# The robot can remember different types of information in different boxes
