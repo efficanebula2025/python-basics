bafb23bfc69a7316e597fcd9fd83aaa7dbc89ec1
efficanebula2025 <efficanebula@gmail.com>
2025-09-18 12:40:45 +0530

    Create Hello.py

---
+++ Hello.py
@@ -0,0 +1,9 @@
+print("Hello, I am learning to program robots!")
+
+
+
+#How this works:
+#print() is a command that means "say something out loud"
+#The text in quotes "..." is exactly what the robot will say
+#Python reads this and makes the robot display the message
+#It's like teaching a robot to speak!
